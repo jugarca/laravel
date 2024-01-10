47cb2689ae6c98ba1a693e97c0ac81b191b25125
Judit <jugarca08@gmail.com>
2024-01-10 13:56:14 +0100

    Controladores y Routes

---
+++ publicaciones/publicaciones/app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        return 'index';
+    }
+
+    public function store(){
+       //guardar
+    }
+
+    public function destroy(){
+        //eliminar
+     }
+}
--- publicaciones/publicaciones/resources/views/layouts/navigation.blade.php
+++ publicaciones/publicaciones/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                        {{ __('Post') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -82,6 +85,10 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('posts.index')">
+                    {{ __('Posts') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
