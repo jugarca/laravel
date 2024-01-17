90acc3a2423d0795793b4daa83bdf0b6bf10958b
Judit <jugarca08@gmail.com>
2024-01-17 23:25:56 +0100

    Filtrado de publicaciones.

--- publicaciones/publicaciones/routes/web.php
+++ publicaciones/publicaciones/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ Route::redirect('/', 'dashboard');
 Route::get('dashboard',[PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::post('posts',[PostController::class, 'store'])->middleware(['auth', 'verified'])->name('posts.store');
 Route::delete('posts/{post}',[PostController::class, 'destroy'])->middleware(['auth', 'verified'])->name('posts.destroy');
+
+Route::get('user/{user}',[UserController::class, 'show'])->middleware(['auth', 'verified'])->name('users.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
